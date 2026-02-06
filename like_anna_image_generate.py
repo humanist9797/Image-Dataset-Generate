0186ddb71c36659add0c8968d57f43a067be64ca
Booza97 <31425956+humanist9797@users.noreply.github.com>
2026-02-06 16:42:46 +0900

    값 수정

--- like_anna_image_generate.py
+++ like_anna_image_generate.py
@@ -8,8 +8,8 @@ output_dir = 'D:/image/dataset1/like_anna_ouput'  # Directory to save the result
 os.makedirs(output_dir, exist_ok=True)
 
 # Set noise intensity range (standard deviation, randomly selected within min~max range)
-min_noise_std = 250 # Minimum noise intensity
-max_noise_std = 500  # Maximum noise intensity
+min_noise_std = 100 # Minimum noise intensity
+max_noise_std = 150  # Maximum noise intensity
 
 # Number of noisy images to generate
 num_images = 1500
